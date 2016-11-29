99810de1737a864ed4875149bf54d58c7a022b38
lgarcia <lgarcia@PB1-34>
2016-11-29 13:20:11 +0100

    Add project files.

---
+++ src/CandyCrush/MenuScene.h
@@ -0,0 +1,22 @@
+/******************************************************************
+* Copyright (C) 2016 Jordi Serrano Berbel <jsberbel95@gmail.com> *
+* This can not be copied, modified and/or distributed without    *
+* express permission of the copyright owner.                     *
+******************************************************************/
+
+#pragma once
+#include "Sprite.hh"
+#include "Scene.hh"
+
+// MenuScene class with the main gameplay mode
+class MenuScene : public Scene {
+public:
+	explicit MenuScene();
+	~MenuScene() override;
+	void OnEntry(void) override;
+	void OnExit(void) override;
+	void Update(void) override;
+	void Draw(void) override;
+private:
+	Sprite m_background;
+};
